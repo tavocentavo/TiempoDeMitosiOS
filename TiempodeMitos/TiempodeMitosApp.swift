db1ee6db49bbc64b30f169ce14369316fac09752
Luis Oyervides <lgoa.770@gmail.com>
2024-02-17 02:19:58 -0600

    Initial Commit

---
+++ TiempodeMitos/TiempodeMitosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiempodeMitosApp.swift
+//  TiempodeMitos
+//
+//  Created by luisoyervides on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TiempodeMitosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
